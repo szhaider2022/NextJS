28670b236975ad38c35c01fd572b22001068d679
zeeshan <hyder.zeeshan110@gmail.com>
2024-09-08 21:39:09 +0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,37 @@
+import OrderButton from "./OrderButton"
+
+interface IBook {
+  id: number,
+  name: string,
+  type: string,
+  available: boolean
+}
+
+async function Homepage() {
+
+  let baseUrl = "https://simple-books-api.glitch.me"
+
+  let res = await fetch(`${baseUrl}/books`)
+
+  let books: IBook[] = await res.json()
+
+
+  return (
+    <div> 
+      <h1>Books</h1>
+      <ul>
+        {books.map((b) => {
+            return (
+              <li key={b.id} className="p-3 text-lg border" >
+                {b.name}
+                <OrderButton />
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Homepage
